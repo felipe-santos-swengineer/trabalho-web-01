8b961d652f841017049b300c02545dd5c2a692c7
Rene-Michel99 <renemichel856@gmail.com>
2021-06-09 19:22:03 -0300

    adesao do pop up

---
+++ pages/script.js
@@ -0,0 +1,7 @@
+function pop_up(){
+  let content = "DESENVOLVIMENTO DE SOFTWARE PARA WEB\n";
+  content += "-----------------------------------------------------------\n";
+  content += "     Renê Michel Santana Padilha\n";
+  content += "     José Felipe Santos Rodrigues\n";
+  alert(content);
+}
